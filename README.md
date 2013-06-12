30352abc68af175191b4e54b1c96394eae4916e1
Caleb R. Begly <calebbegly@gmail.com>
2013-06-12 11:32:57 -0600

    Added Support for allowing select absolute urls to stay.
    Allows support for subdomains to stay inside of the app.

--- README.md
+++ README.md
@@ -12,8 +12,21 @@ This use only links with the class `stay`.
 	
 Full links (starting with http) will still open in Safari, as well as links with `target="_blank"`.
 
+To prevent an absolute link from opening in safari, add its domain to the absToStay array property of the options object like this:
+
+	$(function() {
+		var options = {
+			selector: "a.stay",
+			absToStay: Array("google.com","test.com","cats"), //allow google.com, test.com, and urls containing the word cats to stay.
+		};
+		$.stayInWebApp(options);
+	});
+
+This can help if you have multiple subdomains eg, test.com, sub.test.com, othersub.test.com. All can be allowed by adding test.com to the array.
 - - - - - - - - - -	
 
 Thanks to [Bryan Murdaugh and David Leininger](http://fivable.com) for the original starting script (mostly for discovering that self.location works).
 
 Thanks to Ben Nadel for posting about how to [detect iOS full screen mode](http://www.bennadel.com/blog/1950-Detecting-iPhone-s-App-Mode-Full-Screen-Mode-For-Web-Applications.htm).
+
+Stay for certain absolute urls added by Caleb R. Begly (techdude)
--- jquery.stayInWebApp.js
+++ jquery.stayInWebApp.js
@@ -1,39 +1,57 @@
 /*!
  * jQuery stayInWebApp Plugin
+ https://github.com/mrmoses/jQuery.stayInWebApp
+ * keeps all internal links in the webapp
  * version: 0.4 (2012-06-19)
+ commits mmoses and techdude
  */
- 
-;(function($) {
+(function($) {
 	//extend the jQuery object, adding $.stayInWebApp() as a function
 	$.extend({
-		stayInWebApp: function(selector) {
+		stayInWebApp: function(options) {
+			if(typeof options=="string"){ //we were not passed an object, but only a selector
+				//make object
+				options = {selector:options};
+			}
 			//detect iOS full screen mode
+			console.log(options);
 			if(("standalone" in window.navigator) && window.navigator.standalone) {
 				//if the selector is empty, default to all links
-				if(!selector) {
-					selector = 'a';
+				if(!options.selector) {
+					options.selector = 'a';
 				}
 				//bind to the click event of all specified elements
-				$("body").delegate(selector,"click",function(event) {
-					//TODO: execute all other events if this element has more bound events
-					/* NEEDS TESTING
-					for(i = 0; i < $(this).data('events'); i++) {
-						console.log($(this).data('events'));
-					}
-					*/
-				
+				$(options.selector).click(function(event) {
+					
 					//only stay in web app for links that are set to _self (or not set)
 					if($(this).attr("target") == undefined || $(this).attr("target") == "" || $(this).attr("target") == "_self") {
 						//get the destination of the link clicked
 						var dest = $(this).attr("href");
+						var stay = false; //by default, we do not stay.
 
-						//if the destination is an absolute url, ignore it
-						if(!dest.match(/^http(s?)/g)) {
+						if(!dest.match(/^http(s?)/g)) { //local urls, keep
+							stay = true;
+						  
+						} else if(options.absToStay) { //if some absolute urls must stay
+							
+							
+							//loop throught urls and see if this is one of the ones we should keep
+							for(var i = 0; i < options.absToStay.length; i++){
+								if(dest.indexOf(options.absToStay[i]) > 0){
+									stay = true;
+									break;
+								}
+							}
+						}
+						
+						if(stay){ //if we should stay for this url,
 						  //prevent default behavior (opening safari)
 						  event.preventDefault();
 						  //update location of the web app
 						  self.location = dest;
 						}
+						
+						
 					}
 				});
 			}
